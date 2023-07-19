fee4a45b47bb17c38dcf2d8215e7d6063f952bb9
Karim Shaloh <karimshaloh@Karims-MacBook-Pro.local>
2023-07-19 19:42:52 -0400

    add contacts and invoices pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { ColorModeContext, useMode } from './theme';
-import { CssBaseline, ThemeProvider, Box } from '@mui/material';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Topbar from './scenes/global/Topbar';
 import Sidebar from './scenes/global/Sidebar';
 import Dashboard from './scenes/dashboard/';
+import Invoices from './scenes/invoices';
 import Team from './scenes/team';
-// import Invoices from './scenes/invoices';
-// import Contacts from './scenes/contacts';
+import Contacts from './scenes/contacts';
 // import Bar from './scenes/bar';
 // import Form from './scenes/form';
 // import Line from './scenes/line';
@@ -27,21 +27,19 @@ function App() {
 				<div className='app'>
 					<Sidebar isSidebar={isSidebar} />
 					<main className='content'>
-						<Box>
-							<Topbar setIsSidebar={setIsSidebar} />
-						</Box>
+						<Topbar setIsSidebar={setIsSidebar} />
 						<Routes>
 							<Route path='/' element={<Dashboard />} />
 							<Route path='/team' element={<Team />} />
-							{/* <Route path='/contacts' element={<Contacts />} />
+							<Route path='/contacts' element={<Contacts />} />
 							<Route path='/invoices' element={<Invoices />} />
-							<Route path='/form' element={<Form />} />
-							<Route path='/bar' element={<Bar />} />
-							<Route path='/pie' element={<Pie />} />
-							<Route path='/line' element={<Line />} />
-							<Route path='/FAQ' element={<FAQ />} />
-							<Route path='/geography' element={<Geography />} />
-							<Route path='/calendar' element={<Calendar />} /> */}
+							{/* <Route path='/form' element={<Form />} /> */}
+							{/* <Route path='/bar' element={<Bar />} /> */}
+							{/* <Route path='/pie' element={<Pie />} /> */}
+							{/* <Route path='/line' element={<Line />} /> */}
+							{/* <Route path='/FAQ' element={<FAQ />} /> */}
+							{/* <Route path='/geography' element={<Geography />} /> */}
+							{/* <Route path='/calendar' element={<Calendar />} /> */}
 						</Routes>
 					</main>
 				</div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,19 +5,16 @@ const Header = ({ title, subtitle }) => {
 	const theme = useTheme();
 	const colors = tokens(theme.palette.mode);
 	return (
-		<Box>
+		<Box mb='30px'>
 			<Typography
 				variant='h2'
 				color={colors.grey[100]}
-				font-weight='bold'
+				fontWeight='bold'
 				sx={{ mb: '5px' }}
 			>
 				{title}
 			</Typography>
-			<Typography
-				variant='h5'
-				color={colors.greenAccent[400]}
-			>
+			<Typography variant='h5' color={colors.greenAccent[400]}>
 				{subtitle}
 			</Typography>
 		</Box>
