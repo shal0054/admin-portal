31cb2c31fb5d82e52c5e6d079a07f081fc795ff6
Karim Shaloh <karimshaloh@Karims-MacBook-Pro.local>
2023-07-21 00:15:47 -0400

    all components complete

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -4,15 +4,8 @@ import Header from '../../components/Header';
 const Dashboard = () => {
 	return (
 		<Box m='20px'>
-			<Box
-				display='flex'
-				justifyContent='space-between'
-				alignItems='center'
-			>
-				<Header
-					title='DASHBOARD'
-					subtitle='Welcome to your Dashboard'
-				/>
+			<Box display='flex' justifyContent='space-between' alignItems='center'>
+				<Header title='DASHBOARD' subtitle='Welcome to your Dashboard' />
 			</Box>
 		</Box>
 	);
---
+++ src/scenes/faq/index.jsx
@@ -0,0 +1,113 @@
+import { Box, useTheme } from '@mui/material';
+import Header from '../../components/Header';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { tokens } from '../../theme';
+import { color } from '@mui/system';
+
+const FAQ = () => {
+	const theme = useTheme();
+	const colors = tokens(theme.palette.mode);
+
+	return (
+		<Box m='20px'>
+			<Header title='FAQ' subtitle='Frequently asked questions' />
+
+			<Accordion defaultExpanded>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant='h5'>
+						An Important Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+						cum tempore amet itaque repellat ratione soluta aperiam vel, impedit
+						distinctio non ducimus rerum voluptatibus vero nostrum quae tempora
+						nam odio.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant='h5'>
+						Another Important Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+						cum tempore amet itaque repellat ratione soluta aperiam vel, impedit
+						distinctio non ducimus rerum voluptatibus vero nostrum quae tempora
+						nam odio.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant='h5'>
+						The Most Important Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+						cum tempore amet itaque repellat ratione soluta aperiam vel, impedit
+						distinctio non ducimus rerum voluptatibus vero nostrum quae tempora
+						nam odio.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant='h5'>
+						A Common Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+						cum tempore amet itaque repellat ratione soluta aperiam vel, impedit
+						distinctio non ducimus rerum voluptatibus vero nostrum quae tempora
+						nam odio.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant='h5'>
+						Your Favorite Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+						cum tempore amet itaque repellat ratione soluta aperiam vel, impedit
+						distinctio non ducimus rerum voluptatibus vero nostrum quae tempora
+						nam odio.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+			<Accordion>
+				<AccordionSummary expandIcon={<ExpandMoreIcon />}>
+					<Typography color={colors.greenAccent[500]} variant='h5'>
+						The Final Question
+					</Typography>
+				</AccordionSummary>
+				<AccordionDetails>
+					<Typography>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+						cum tempore amet itaque repellat ratione soluta aperiam vel, impedit
+						distinctio non ducimus rerum voluptatibus vero nostrum quae tempora
+						nam odio.
+					</Typography>
+				</AccordionDetails>
+			</Accordion>
+		</Box>
+	);
+};
+
+export default FAQ;
