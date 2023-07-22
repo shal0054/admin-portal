dc5b3dbdab7c1010f5bfbb3b1823e592bab3fb66
Karim Shaloh <karimshaloh@Karims-MacBook-Pro.local>
2023-07-22 00:54:30 -0400

    code clean up

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -6,7 +6,6 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { tokens } from '../../theme';
-import { color } from '@mui/system';
 
 const FAQ = () => {
 	const theme = useTheme();
